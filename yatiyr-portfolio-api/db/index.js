9176a7d0ba04766f5837333643fe3cdd57330877
yatiyr <dereeren@yahoo.com>
2021-09-12 21:16:57 +0300

    api project added for backend operations. I now have a custom mongodb database server on amazon aws.

---
+++ yatiyr-portfolio-api/db/index.js
@@ -0,0 +1,16 @@
+const config = require('../config/dev');
+const mongoose = require('mongoose');
+
+require('./models/blog');
+
+exports.connect = () => {
+    return mongoose.connect(config.DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+        }, (err) => {
+            if(err) {console.error(err);}
+            else {
+                console.log('Connected to DB!');
+            }
+        });
+}
