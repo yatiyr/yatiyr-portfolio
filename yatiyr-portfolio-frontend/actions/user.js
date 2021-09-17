bf6ac2283cc377fcacc0cd0e98b5901d6b0ee690
yatiyr <dereeren@yahoo.com>
2021-09-17 23:42:31 +0300

    api is implemented

--- yatiyr-portfolio-frontend/actions/user.js
+++ yatiyr-portfolio-frontend/actions/user.js
@@ -1,5 +1,5 @@
 import useSWR from "swr";
-import {fecther} from "actions";
+import {fetcher} from "actions";
 
 export const useGetUser = () => {
     const {data, error, ...rest} = useSWR('/api/v1/me', fetcher);
