a19bafdfbff9e562e69ea5a2b17b2b0864678207
yatiyr <dereeren@yahoo.com>
2021-09-28 00:58:23 +0300

    unnecesarry response parts removed from api calls.

--- yatiyr-portfolio-api/controllers/blogs.js
+++ yatiyr-portfolio-api/controllers/blogs.js
@@ -3,12 +3,43 @@ const Blog       = mongoose.model('Blog');
 
 exports.getBlogs = async (req, res) => {
     const blogs = await Blog.find({}).sort({publishedAt: -1});
-    return res.json(blogs);
+    const reducedBlogs = blogs.reduce((allBlogs, blogData) => {
+        return [
+            {
+                slug: blogData.slug,
+                title: blogData.title,
+                publishedAt: blogData.publishedAt,
+                summary: blogData.summary,
+                headImageUrl: blogData.headImageUrl,
+                highlighted: blogData.highlighted,
+                postType: blogData.postType,
+                updatedAt: blogData.updatedAt
+            },
+            ...allBlogs
+        ]
+    }, [])
+
+    return res.json(reducedBlogs);
 }
 
 exports.getHighlightedBlogs = async (req, res) => {
     const blogs = await Blog.find({highlighted: "true"}).sort({publishedAt: -1});
-    return res.json(blogs);
+    const reducedBlogs = blogs.reduce((allBlogs, blogData) => {
+        return [
+            {
+                slug: blogData.slug,
+                title: blogData.title,
+                publishedAt: blogData.publishedAt,
+                summary: blogData.summary,
+                headImageUrl: blogData.headImageUrl,
+                highlighted: blogData.highlighted,
+                postType: blogData.postType,
+                updatedAt: blogData.updatedAt
+            },
+            ...allBlogs
+        ]
+    }, [])    
+    return res.json(reducedBlogs);
 }
 
 exports.getBlogBySlug = async (req, res) => {
